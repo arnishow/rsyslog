ceaea6e44e4c46c13729b3d30d36d6fb0340198c
arnishow <arnishow@gmail.com>
2017-06-01 22:32:01 +0200

    .

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ if [ -n "$DB" ] [ -n "$DB_NAME" ] && [ -n "$DB_USER" ] && [ -n "$DB_PWD" ]; then
   echo "*.* :ommysql:$DB,$DB_NAME,$DB_USER,$DB_PWD" >> /etc/rsyslog.conf
 fi
 
-exec exec rsyslogd -n
+exec rsyslogd -n
